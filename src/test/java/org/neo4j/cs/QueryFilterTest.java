a8f2d84d0ea95b4c17f6308d4934af686de37c13
David Pecollet <david.pecollet@gmail.com>
2024-01-10 17:18:05 +0000

    tests

---
+++ src/test/java/org/neo4j/cs/QueryFilterTest.java
@@ -0,0 +1,61 @@
+package org.neo4j.cs;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueryFilterTest {
+
+    @Test
+    void shouldFilterExplainQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("EXPLAIN MATCH(n) RETURN n");
+        queries.add("  EXPLAIN   MATCH(n) RETURN n");
+        List<String> filtered =  new QueryFilter().filter(queries);
+        assertTrue(filtered.isEmpty());
+    }
+
+    @Test
+    void shouldFilterShowQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("SHOW DATABASES");
+        queries.add(" show servers");
+        List<String> filtered =  new QueryFilter().filter(queries);
+        assertTrue(filtered.isEmpty());
+    }
+
+    @Test
+    void shouldFilterIgnoreQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("This query is just used to load the cypher compiler during warmup. Please ignore");
+        List<String> filtered =  new QueryFilter().filter(queries);
+        assertTrue(filtered.isEmpty());
+    }
+
+    @Test
+    void shouldCleanupProfileQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("PROFILE MATCH(n) RETURN n");
+        queries.add(" profile MATCH(x) RETURN x");
+        List<String> filtered =  new QueryFilter().filter(queries);
+        assertTrue(filtered.contains("MATCH(n) RETURN n"));
+        assertTrue(filtered.contains("MATCH(x) RETURN x"));
+    }
+
+    @Test
+    void shouldCleanupPrefixedQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("CYPHER RUNTIME=SLOTTED MATCH(n) RETURN n");
+        queries.add("CYPHER runtime=interpreted MATCH(x) RETURN x");
+        queries.add(" CYPHER   runtime=PARALLEL   MATCH(y) RETURN y");
+        queries.add("CYPHER RUNTIME=SLOTTED   expressionEngine=INTERPRETED MATCH(z) RETURN z");
+        List<String> filtered =  new QueryFilter().filter(queries);
+        assertTrue(filtered.contains("MATCH(n) RETURN n"));
+        assertTrue(filtered.contains("MATCH(x) RETURN x"));
+        assertTrue(filtered.contains("MATCH(y) RETURN y"));
+        assertTrue(filtered.contains("MATCH(z) RETURN z"));
+    }
+
+}
---
+++ src/test/java/org/neo4j/cs/QueryParserTest.java
@@ -0,0 +1,40 @@
+package org.neo4j.cs;
+
+import org.junit.jupiter.api.Test;
+import org.neo4j.cs.model.Model;
+import org.neo4j.cs.model.Property;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class QueryParserTest {
+    @Test
+    void shouldParseQueries() {
+        List<String> queries = new ArrayList<>();
+        queries.add("MATCH(t:Thing)-[:HAS]->(:Stuff) WHERE t.name= 'thingy' RETURN t");
+        queries.add("MATCH(s:Stuff)-[:IS]->(:Class) RETURN n");
+        Model m = new QueryParser().parseQueries(queries);
+
+        Set expectedNodeLabels = new HashSet<String>();
+        expectedNodeLabels.add("Thing");
+        expectedNodeLabels.add("Stuff");
+        expectedNodeLabels.add("Class");
+
+        Set expectedRelTypes = new HashSet<String>();
+        expectedRelTypes.add("HAS");
+        expectedRelTypes.add("IS");
+
+        Set expectedProperties = new HashSet<String>();
+        expectedProperties.add(new Property("name"));
+
+        assertEquals(m.getNodeLabels().keySet(), expectedNodeLabels);
+        assertEquals(m.getRelationshipTypes().keySet(), expectedRelTypes);
+        assertEquals(m.getNodeLabels().get("Thing").getProperties(), expectedProperties);
+    }
+
+}
