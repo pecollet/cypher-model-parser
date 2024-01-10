5cc7a4cbfadd63efaa84285d4a1d809944798169
David Pecollet <david.pecollet@gmail.com>
2024-01-10 17:35:19 +0000

    fixed maven tests

---
+++ src/test/java/org/neo4j/cs/HcQueryMapCsvReaderTest.java
@@ -0,0 +1,18 @@
+package org.neo4j.cs;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HcQueryMapCsvReaderTest {
+    @Test
+    void shouldReadNormalQueries() {
+        QueryFileReader queryReader = new HcQueryMapCsvReader(new QueryFilter());
+        List<String> queries = queryReader.read("src/test/resources/queries_map_1.csv");
+        assertTrue(queries.size() == 9);
+        assertTrue(queries.contains("MATCH (t:Thing) RETURN t"));
+        assertTrue(queries.contains("MATCH (this1:A:B:C:D)-[:multi]->(:ff) \nreturn this1"));
+    }
+}
