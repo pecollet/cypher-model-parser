7ec7b2d18d65dfb84f973744c4dde2a28b08bc38
David Pecollet <david.pecollet@gmail.com>
2026-02-04 11:12:48 +0000

    feat: added backwards compatibility for obfuscator dialects option

--- src/main/java/org/neo4j/cs/Obfuscator.java
+++ src/main/java/org/neo4j/cs/Obfuscator.java
@@ -12,10 +12,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.neo4j.cs.QueryFilter.*;
 
@@ -27,11 +30,42 @@ public class Obfuscator implements Callable<Integer>  {
     static class Dialects extends ArrayList<String> {
         Dialects() {
             super( Arrays.stream(CypherVersion.values())
-                    .map(CypherVersion::name)
+                    .map(Object::toString)
                     .collect(Collectors.toList())
             );
         }
     }
+    // for backwards compatibility with version <2 of this tool
+    static final Set<String> CYPHER_DSL_ALIASES = Set.of(
+            "NEO4J_5_23", "NEO4J_4", "NEO4J_5", "NEO4J_5_26"
+    );
+    static class CypherDialectConverter
+            implements CommandLine.ITypeConverter<CypherVersion> {
+        private static final Map<String, CypherVersion> LOOKUP =
+                Arrays.stream(CypherVersion.values())
+                        .collect(Collectors.toMap(
+                                v -> v.toString(),  // "5", "25", ...
+                                v -> v
+                        ));
+        @Override
+        public CypherVersion convert(String value) {
+            String normalized = value.trim();
+            // try proper CypherVersion values ("5", "25") first
+            CypherVersion v = LOOKUP.get(normalized);
+            if (v != null) return v;
+
+            // allow cypher-dsl dialects (for backwards compatibility), but always map to 5
+            if (CYPHER_DSL_ALIASES.contains(normalized)) {
+                return CypherVersion.Cypher5;
+            }
+            // Reject everything else
+            throw new CommandLine.TypeConversionException(
+                    "Invalid dialect: " + value +
+                            ". Expected one of: " + LOOKUP.keySet() + " or, for backwards compatibility, one of : " + CYPHER_DSL_ALIASES + " (which all map to '5')"
+            );
+        }
+    }
+
 //    private static final String OBFUSCATED_STRING = "****"; // Replace with the desired obfuscated text
 
     @CommandLine.ArgGroup(exclusive = true, multiplicity = "1")
@@ -49,7 +83,8 @@ public class Obfuscator implements Callable<Integer>  {
     @CommandLine.Option(names = { "-o", "--output" }, description = "Output file generated, containing the obfuscated query.")
     private Path outputFile;
 
-    @CommandLine.Option(names = { "-d", "--dialect" }, paramLabel = "dialect",  completionCandidates = Dialects.class, defaultValue = "25", description = "The cypher dialect, one of : [${COMPLETION-CANDIDATES}]. Defaults to 25.")
+    @CommandLine.Option(names = { "-d", "--dialect" }, paramLabel = "dialect",  completionCandidates = Dialects.class,
+            defaultValue = "25", converter = CypherDialectConverter.class, description = "The cypher dialect, one of : [${COMPLETION-CANDIDATES}]. Defaults to 25.")
     private CypherVersion dialect;
 
     @CommandLine.Option(names = { "-p", "--pretty" }, description = "Always pretty print the resulting cypher, even if no obfuscation took place. Obfuscated cypher will be pretty printed in any case.")
