f9f9327d191118aba5181a41180ab725e6aa0abe
David Pecollet <david.pecollet@gmail.com>
2026-02-18 12:16:40 +0000

    added test for parsing CYPHER 25 prefixed queries

--- src/test/java/org/neo4j/cs/HcQueryMapCsvReaderTest.java
+++ src/test/java/org/neo4j/cs/HcQueryMapCsvReaderTest.java
@@ -13,8 +13,9 @@ public class HcQueryMapCsvReaderTest {
         QueryFileReader queryReader = new HcDatabaseQueriesCsvReader(new QueryFilter());
         List<String> queries = queryReader.read(new File("src/test/resources/database_queries_1.csv"));
 
-        assertTrue(queries.size() == 9);
+        assertEquals(queries.size(),  10);
         assertTrue(queries.contains("MATCH (t:Thing) RETURN t"));
         assertTrue(queries.contains("MATCH (this1:A:B:C:D)-[:multi]->(:ff) \nreturn this1"));
+        assertTrue(queries.contains("LET x = 1 MATCH (n:Node) WHERE n.name = x RETURN n"));
     }
 }
